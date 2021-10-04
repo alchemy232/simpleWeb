1c0a38cb8e9330d4b5c35f481c5f683674f0fe5d
alchemy232 <mr.alchemy@gmail.com>
2021-10-04 15:02:17 +0300

    Initial commit

---
+++ src/main/java/ru/alchemy/simpleWeb/SimpleWebApplication.java
@@ -0,0 +1,13 @@
+package ru.alchemy.simpleWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebApplication.class, args);
+	}
+
+}
